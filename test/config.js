be5cc2e1153556a833d9d434602aaa63f42b712b
unknown <oliver.foster@LenovoThinkcent.kineo.local>
2014-07-04 10:34:54 +0100

    Trimmed unit tests

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
  new adaptTester({
     run : true,
-    //url : "../../../../build",
+    url : "../../../../build",
     //undefined :               for test to ask which build url to use, popup
     //"../build" : 				for	build unit tests
     //"../../../../build" : 	for component/extension/menu unit tests
@@ -9,11 +9,6 @@
         	"name" : "search",
         	"url" : "spec/test",
         	"runat" : "start"
-        },
-        {
-            "name" : "clicker",
-            "url" : "spec/test2",
-            "runat" : "click"
         }
     ]
 });
